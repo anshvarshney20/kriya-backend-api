4b573a27e272f992e22dd3734cfb4ff57111a4fe
PyCode Hub <81847852+anshvarshney20@users.noreply.github.com>
2024-01-24 21:07:54 +0530

    Update database.py

--- auth/database.py
+++ auth/database.py
@@ -1,7 +1,9 @@
 from pymongo import MongoClient
 
 # Create a MongoDB client
-client = MongoClient("mongodb+srv://anshvarshney20libra:ansh2012@cluster0.2ta0nkk.mongodb.net/?retryWrites=true&w=majority")
+# client = MongoClient("mongodb+srv://anshvarshney20libra:ansh2012@cluster0.2ta0nkk.mongodb.net/?retryWrites=true&w=majority")
+client = MongoClient("mongodb://mongo:Fb31ABebaAhF32a66hc-D-AeEccd3bDG@monorail.proxy.rlwy.net:57476")
+
 # client = MongoClient("mongodb://mongo:aeF-De3d2dE661Ghc44cF4GCgeDEafdH@viaduct.proxy.rlwy.net:40070")
 # Access the database and collection
 db = client.todo_db
