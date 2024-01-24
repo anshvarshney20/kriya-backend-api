7f21d17b90e6ee97c710833b131b04b76f3a601d
anshvarshney20 <ansh.varshneydec@gmail.com>
2024-01-24 20:36:48 +0530

    hosting

--- auth/database.py
+++ auth/database.py
@@ -1,8 +1,8 @@
 from pymongo import MongoClient
 
 # Create a MongoDB client
-# client = MongoClient("mongodb+srv://anshvarshney20libra:ansh2012@cluster0.2ta0nkk.mongodb.net/?retryWrites=true&w=majority")
-client = MongoClient("mongodb://mongo:aeF-De3d2dE661Ghc44cF4GCgeDEafdH@viaduct.proxy.rlwy.net:40070")
+client = MongoClient("mongodb+srv://anshvarshney20libra:ansh2012@cluster0.2ta0nkk.mongodb.net/?retryWrites=true&w=majority")
+# client = MongoClient("mongodb://mongo:aeF-De3d2dE661Ghc44cF4GCgeDEafdH@viaduct.proxy.rlwy.net:40070")
 # Access the database and collection
 db = client.todo_db
 # collection_name = db["todo_collection"]
